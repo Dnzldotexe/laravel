aecee78c46639562f8c24de36635cae0770c40e5
Danziel Cempron <104302220+Dnzldotexe@users.noreply.github.com>
2024-10-01 03:09:15 +0000

    Route to root

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\URL;
 
 $url = config('app.url');
-URL::forceRouteUrl($url);
+URL::forceRootUrl($url);
 
 Route::get('/', function () {
     return view('welcome');
